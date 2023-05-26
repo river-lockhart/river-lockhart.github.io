181a26d028a38ce34a3e341d71f7d30463841663
River <109698282+river-lockhart@users.noreply.github.com>
2023-05-26 16:22:15 -0500

    Updated initial animation

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-const letters = document.querySelectorAll("li");
 
-setTimeout(() => {
-        Array.from(letters).forEach((letter,index) => {
-                setTimeout(() => {
-                        letter.classList.toggle("show");
-                }, index * 150);
-        })
-}, 1000);
+
